099e57867db7fac26f7cf72adf90c44979cab050
krgarav <krgarav@gmail.com>
2023-07-06 07:18:40 +0530

    Dynamic route implemented

--- Models/product.js
+++ Models/product.js
@@ -1,35 +1,50 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const rootDir = require("../util/path");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const p = path.join(rootDir, "Data", "products.json");
-
-const getAllProducts = (cb) => {
-  let product = [];
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
-    if (!err) {
-      product = JSON.parse(fileContent);
-      cb(product);
+    if (err) {
+      cb([]);
     } else {
-      cb(product);
+      cb(JSON.parse(fileContent));
     }
   });
 };
+
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
+
   save() {
-    getAllProducts((product) => {
-      product.push(this);
-      fs.writeFile(p, JSON.stringify(product), (err) => {
+    this.id=Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
-    getAllProducts(cb);
+    getProductsFromFile(cb);
+  }
+
+  static getProduct(id,cb){
+    getProductsFromFile(products=>{
+      const product=products.find(p=>p.id===id);
+     
+      cb(product);
+    });
+
   }
 };
